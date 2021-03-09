9283df07a3c0df1207e11e48092ac78b3c66c687
Mateusz Rorat <mateuszrorat@gmail.com>
2021-03-09 22:30:16 +0100

    add more excluded extensions

--- src/app/crawler/fetchCondition.ts
+++ src/app/crawler/fetchCondition.ts
@@ -4,7 +4,7 @@ import * as url from 'url';
 export const addFetchCondition = function (queueItem, next) {
   if (
     queueItem.path.match(
-      /\.(css|jpg|jpeg|pdf|docx|js|png|ico|xml|svg|mp3|mp4|gif|exe|swf|woff|eot|ttf)/i
+      /\.(css|jpg|jpeg|pdf|docx|js|png|ico|xml|svg|mp3|mp4|gif|exe|swf|woff|eot|ttf|ppt|pptx|doc|docx|ogv|dmg|webm|mov|bmp|apk|air|zip|rar|dot|axd|)/i
     )
   ) {
     log('SKIPPED: ' + queueItem.path);
